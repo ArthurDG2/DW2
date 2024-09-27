aabc412033a4e07683453a12ef029111c86e1349
ArthurDG2 <140561819+ArthurDG2@users.noreply.github.com>
2024-09-27 00:07:17 -0300

    Finalizações

--- src/controllers/produto.controller.js
+++ src/controllers/produto.controller.js
@@ -3,7 +3,11 @@ import Produto from '../models/produto.model.js'
 
 export default class ProdutoController{
   static async index(req, res) {
-    const produtos = await Produto.findMany()
+    const produtos = await Produto.findMany({
+      include: {
+        fotos: true
+      }
+    })
     res.json(produtos)
   }
 
@@ -27,7 +31,12 @@ export default class ProdutoController{
       const produto = await Produto.findUnique({
         where: {
           id: parseInt(req.params.id)
-        }
+        }, 
+
+          include: {
+            fotos: true
+          }
+      
       })
       if (!produto) {
         return res.status(404).json({ message: 'Produto não encontrado' })
